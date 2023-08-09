07cd54997455ac4a53cf10435fbe396f45e4852d
Edgar Melara <e.melara@uniandes.edu.co>
2023-08-08 21:34:48 -0600

    feat: create base proyect

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const testMessage = screen.getByText("Hello world");
+  expect(testMessage).toBeInTheDocument();
 });
